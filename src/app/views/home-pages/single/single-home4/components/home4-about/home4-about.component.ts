10260a9a0e32fca34814a81accac5efab219d2f7
Inara Oenning <inaraoenning@gmail.com>
2025-08-25 15:15:44 -0300

    feat: adicionar botão de scroll no sobre nos e alterar cor do SVG animado

--- src/app/views/home-pages/single/single-home4/components/home4-about/home4-about.component.ts
+++ src/app/views/home-pages/single/single-home4/components/home4-about/home4-about.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ScrollService } from '@/core/services/scroll.service';
 
 @Component({
   selector: 'app-home4-about',
   standalone:true,
   host: { 'data-component-id': 'home4-about-instance' },
-  imports: [RouterLink],
   templateUrl: './home4-about.component.html',
   styles: ``
 })
 export class Home4AboutComponent {
+  public scroll =  inject(ScrollService);
 
 }
