c845b45565aa2881c8b39c56277121f20a5130e8
Feyre_head <inaraoenning@gmail.com>
2025-08-29 17:34:28 -0300

    Ajustando section cobertura, adicionando mapas região

--- src/app/views/home-pages/single/single-home4/components/home4-cobertura/home4-cobertura.component.ts
+++ src/app/views/home-pages/single/single-home4/components/home4-cobertura/home4-cobertura.component.ts
@@ -1,64 +1,28 @@
-import { GsapRevealDirective } from '@/core/directives/gsap-reveal.directive';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { CarouselModule, type OwlOptions } from 'ngx-owl-carousel-o';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import type { CoberturaType } from '@/types';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
     selector: 'app-home4-cobertura',
     standalone: true,
-    host: { 'data-component-id': 'home4-cobertura-instance' },
-    imports: [CarouselModule, CommonModule, GsapRevealDirective],
-    templateUrl: './home4-cobertura.component.html',
-    styles: ``
+    imports: [CommonModule, CarouselModule],
+    templateUrl: './home4-cobertura.component.html'
 })
 export class Home4CoberturaComponent {
-    
 
-    cobertura: CoberturaType[] = [
-        {
-            image: 'assets/img/all-images/case/case-img6.png',
-            tag: 'Candói/PR',
-            title: 'Rua Manoel Lopes de Oliveira, 2572, Sala 01 - Centro'
-        },
-        {
-            image: 'assets/img/all-images/case/case-img6.png',
-            tag: '#Network Optimization',
-            title: 'Business Continuity with An BackupShield Data Recovery'
-        },
-        {
-            image: 'assets/img/all-images/case/case-img8.png',
-            tag: '#Cyber Security',
-            title: 'Strengthening Security with A TechGuard Cybersecurity'
-        },
-        {
-            image: 'assets/img/all-images/case/case-img5.png',
-            tag: '#Cyber Security',
-            title: 'Strengthening Security with A TechGuard Cybersecurity'
-        }
-    ];
-
-    customOptions: OwlOptions = {
-        loop: true,
+        customOptions: OwlOptions = {
+        loop: false,
         margin: 30,
         nav: true,
         dots: false,
-        items: 10,
-        navText: ["<i class='fa-solid fa-angle-left'></i>", "<i class='fa-solid fa-angle-right'></i>"],
         autoplay: true,
-        smartSpeed: 1500,
+        smartSpeed: 1000,
         autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    }
+        autoHeight: true,                 //altura ao slide
+        navText: [
+            "<i class='fa-solid fa-angle-left'></i>",
+            "<i class='fa-solid fa-angle-right'></i>"
+        ],
+        responsive: { 0: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } }
+    };
 }
